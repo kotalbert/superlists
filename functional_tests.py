ce2b643f65062a61f1e954803e9e2fc876dce82e
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-05-23 20:22:49 +0200

    BLUE: Refactor functional_tests.py
    * Introduce  `unittest'
    * Add setup and cleanup to test case
    
    BLUE: refactor functional_tests.py to use `unittest`

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        User should be able to start a To-Do list and retrieve it later.
+        """
+
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test ???')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')

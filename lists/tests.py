5e0fd8a0f76b37f5d9c4425cd72a2837a9dbc4a0
Pawel Daniluk <pawel@daniluk.waw.pl>
2021-05-23 21:13:47 +0200

    RED: Home page should be resolved

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
+
+class TestHomePage(TestCase):
+
+    def test_resolve_home_page(self):
+        """
+        Root URL should resolve to home page view.
+        """
+
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
